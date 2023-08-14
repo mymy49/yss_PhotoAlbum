0654fd79e08a7afa51882d6c16e5fdd52f7d9029
Yoon-Ki Hong <mymy49@nate.com>
2023-08-14 11:53:01 +0900

    [EXTI 예제 코드 업데이트]

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -24,6 +24,31 @@
 #include <yss.h>
 #include <bsp.h>
 
+triggerId gTriggerId;
+
+// Interrupt Service Routine에서는 문맥 전환을 유발하는 
+// thread::yield(), thread::delay() 등의 함수 호출을하면 안됩니다.
+// 장치의 데이터 전송도 불가능합니다.
+// 위와 같은 작업을 인터럽트와 연계해서 처리하기 위해서는 trigger 기능을 연동하면 됩니다.
+void isr_exti0(void)
+{
+	trigger::run(gTriggerId);
+}
+
+// trigger::run()이 발생되면 첫 쓰레드 진입에서 높은 우선순위로 먼처 스케줄링 됩니다.
+// 처음 1회 높은 우선순위로 스케줄링 된 후, 문맥전환이 발생되면 쓰레드가 return 될 때까지는 일반 라운드로빈 스케줄링됩니다.
+void trigger_blinkLed(void)
+{
+	for(uint32_t i=0;i<5;i++)
+	{
+		Led::on(true);
+		thread::delay(100);
+
+		Led::on(false);
+		thread::delay(100);
+	}
+}
+
 int main(void)
 {
 	// 운영체체 초기화
@@ -31,6 +56,14 @@ int main(void)
 	
 	// 보드 초기화
 	initializeBoard();
+
+	// 외부인터럽트 초기화 (USER 버튼)
+	// PC13에 하강 엣지로 설정
+	exti.add(gpioC, 13, Exti::FALLING, isr_exti0);
+	exti.enable(13);
+	
+	// trigger 등록
+	gTriggerId = trigger::add(trigger_blinkLed, 512);
 
 	while(1)
 	{
